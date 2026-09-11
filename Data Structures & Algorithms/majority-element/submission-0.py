73a5e8d7a0ff7ad0f30380bcfdfe63dcd0bbc907
Misbah43 <72561521+Misbah43@users.noreply.github.com>
2026-09-11 20:24:44 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq=Counter(nums)
+        for key, value in freq.items():
+            if value>len(nums)//2:
+                return key
+
+        
